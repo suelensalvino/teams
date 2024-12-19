e4018784c74894fda4d7f866d9dfb4db3049a68f
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-19 15:40:02 -0300

    add loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+import { Container, LoadIndicator } from "./styles";
+
+export function Loading() {
+  return (
+    <Container>
+      <LoadIndicator />
+    </Container>
+  );
+}
