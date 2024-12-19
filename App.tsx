c4e252cc5ba9b85030ec52f368c3428f5995b28a
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-19 15:28:31 -0300

    custom fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import { Groups } from '@screens/Groups';
-import { ThemeProvider } from 'styled-components/native';
-import theme from '@theme/index';
+import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components/native";
+import theme from "@theme/index";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
-
   );
 }
