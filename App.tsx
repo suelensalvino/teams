4a83861bc9e1df1f5a16529aa2ef5c5d23319915
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-19 15:50:57 -0300

    styling statusbar

--- App.tsx
+++ App.tsx
@@ -7,12 +7,18 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="light-content"
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
