42d2d1fdfbdbce6c30ce458844fe404c27a6b7f3
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-24 10:43:06 -0300

    listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,15 +2,27 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Turma da Faculdade"]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Pessoal do estágio" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard
+          title={item}
+          />
+        )}
+      />
     </Container>
   );
 }
