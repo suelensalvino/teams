d81feb673f8ee918a0aa3535084929f7b0baf397
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-26 09:58:01 -0300

    list empty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(["Turma da Faculdade"]);
@@ -17,10 +18,10 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <GroupCard
-          title={item}
-          />
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
     </Container>
