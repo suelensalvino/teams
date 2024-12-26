a6fe877fc49ef5224942c22126a2910429711073
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-26 11:38:22 -0300

    button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(["Turma da Faculdade"]);
@@ -24,6 +25,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
