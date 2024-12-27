584ca160af3380ae275ae1ee42bbc7376acddd03
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-27 11:22:35 -0300

    input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -13,7 +14,10 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-        <Button title="Criar" />
+        <Input 
+        placeholder="Nome da turma" 
+        />
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
