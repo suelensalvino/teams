fb4456ad36163c7166dca292294ac580d3d91f87
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-30 11:05:29 -0300

    listing the filters

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,15 +1,15 @@
-import { TouchableOpacityProps } from "react-native";
-import { Container, Title, FilterStyleProps } from "./styles";
+import { TouchableOpacityProps } from 'react-native';
+import { Container, Title, FilterStyleProps } from './styles';
 
-type Props = TouchableOpacityProps &
-  FilterStyleProps & {
-    title: string;
-  };
-
+type Props = TouchableOpacityProps & FilterStyleProps & {
+  title: string;
+}
 export function Filter({ title, isActive = false, ...rest }: Props) {
   return (
-    <Container {...rest}>
-      <Title>{title}</Title>
+    <Container isActive={isActive} {...rest}>
+      <Title>
+        {title}
+      </Title>
     </Container>
   );
 }
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
 import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -17,7 +22,21 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time A" />
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
