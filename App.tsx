0cb2a03096c805cc8a0b0258776e505d872e95ed
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-30 16:21:25 -0300

    navigation structure

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { Players } from "@screens/Players";
 import { ThemeProvider } from "styled-components/native";
 import theme from "@theme/index";
 import {
@@ -8,6 +7,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
+import { Routes } from "src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         translucent
         barStyle="light-content"
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
