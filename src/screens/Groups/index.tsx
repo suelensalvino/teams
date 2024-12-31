c583780f566a296ee74ee77ec92085aa3fe6e969
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-31 09:42:54 -0300

    typed routes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,9 +6,15 @@ import { useState } from "react";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(["Turma da Faculdade"]);
+  const navigation = useNavigation();
+  
+  function handleNewGroup() {
+    navigation.navigate("new");
+  }
 
   return (
     <Container>
@@ -25,7 +31,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }
