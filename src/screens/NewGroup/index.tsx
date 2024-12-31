06aa0020268b63b1631337ccc240af6b0e99f5a3
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-31 10:07:33 -0300

    implementing return route

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,15 @@ import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { groups: "Turma da Faculdade" });
+  }
+  
   return (
     <Container>
       <Header showBackButton />
@@ -14,10 +21,8 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-        <Input 
-        placeholder="Nome da turma" 
-        />
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Input placeholder="Nome da turma" />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
