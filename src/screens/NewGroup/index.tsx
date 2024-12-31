21a0598cd8a4a9a6cdde062910b48db7e6d8bdf9
Suelen Salvino <suelensalvinocs@gmail.com>
2024-12-31 10:35:39 -0300

    passing parameters to route

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,12 +4,15 @@ import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function NewGroup() {
+  const [group, setGroup] = useState('');
+
   const navigation = useNavigation();
 
   function handleNew() {
-    navigation.navigate("players", { groups: "Turma da Faculdade" });
+    navigation.navigate("players", { group });
   }
   
   return (
@@ -21,7 +24,10 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input 
+        placeholder="Nome da turma"
+        onChangeText={setGroup} 
+        />
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
