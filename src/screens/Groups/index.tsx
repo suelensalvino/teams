88a2acc85120a3416dc6ebe7c4dbf23fcc21cbfe
Suelen Salvino <suelensalvinocs@gmail.com>
2025-01-02 15:37:10 -0300

    opening the selected group

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -26,6 +26,10 @@ export function Groups() {
     }
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
+
   useFocusEffect(
     useCallback(() => {
       fetchGropus();
@@ -41,7 +45,9 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
